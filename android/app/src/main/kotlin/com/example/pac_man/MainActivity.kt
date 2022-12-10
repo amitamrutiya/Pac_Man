03845b23f64c8bcaa08939997dfa8a6dc8bea1cb
amit patel <amitamrutiya2210@gmail.com>
2022-12-10 09:46:14 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/pac_man/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.pac_man
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+
+import 'package:amit_pac_man/HomePage.dart';
+import 'package:flutter/material.dart';
+
+void main () {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
